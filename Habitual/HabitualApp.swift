dfaf516ea72f3e74794dbbd95e0d5f9f4c370676
Ian Bailey <IanKulin@kulin.com.au>
2022-10-16 15:18:15 +0800

    Initial Commit

---
+++ Habitual/HabitualApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitualApp.swift
+//  Habitual
+//
+//  Created by Ian Bailey on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
